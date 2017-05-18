6f56ddc6689e49c0649b0144ab6f4a103e8bbd0f
Yaqi Zhang <hsbzyq@gmail.com>
2017-05-18 06:37:42 +0000

    update setup

--- src/setup.sh
+++ src/setup.sh
@@ -2,3 +2,7 @@ pip install --user sklearn --upgrade
 pip install --user scipy --upgrade
 pip install --user numpy --upgrade
 pip install --user matplotlib --upgrade
+pip install --user pyyaml --upgrade
+pip install --user Cython --upgrade
+pip install --user easydict --upgrade
+pip install --user Pillow --upgrade
