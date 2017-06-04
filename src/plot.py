db1d201f51dd93d152148aa5d3df65a6794b4d4b
Yaqi Zhang <hsbzyq@gmail.com>
2017-06-04 01:12:47 -0700

    add plot script

--- src/plot.py
+++ src/plot.py
@@ -1,21 +1,81 @@
 from os import listdir
 from os.path import isfile, isdir, join, splitext, basename, dirname
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
+import argparse
 
-RESULT_PATH='../results'
 
-logs = [f for f in listdir(RESULT_PATH) if isfile(join(RESULT_PATH, f)) and f.endswith('.log')]
+def parse(**options):
+    path = options['path']
+    # parse logs
+    global results, logs
+    results = {}
+    logs = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.txt')]
 
-# parse logs
-results = {}
-for log in logs:
-    with open(join(RESULT_PATH, log), 'r') as f:
-        results[log] = {}
-        for line in f:
-            if 'Configuration' in line:
-                key, val = line.split(' ')[1].split('=')
-                results[log][key] = val
-            if 'Error' in line or 'Fail' in line: # broken log
-                results.pop(log, None)
-                continue
+    for log in logs:
+        with open(join(path, log), 'r') as f:
+            results[log] = {}
+            results[log]['epoch'] = []
+            results[log]['train_mse'] = []
+            results[log]['val_mse'] = []
+            for line in f:
+                if 'Configuration' in line:
+                    key, val = line.split(' ')[1].split('=')
+                    results[log][key] = val
+                if 'Error' in line or 'Fail' in line: # broken log
+                    results.pop(log, None)
+                    continue
+                if 'Overall train mse' in line:
+                    results[log]['epoch'].append(int(line.split('Epoch ')[1].split(',')[0]))
+                    results[log]['train_mse'].append(float(line.split('Overall train mse = ')[1].split(',')[0]))
+                    results[log]['val_mse'].append(float(line.split('Overall val mse = ')[1]))
+
+def lookup(d):
+    label = ''
+    if int(d['speedmode'])==0: label += 'flow'
+    elif int(d['speedmode'])==1: label += 'flow+obj'
+    elif int(d['speedmode'])==2: label += 'flow+rgb'
+    elif int(d['speedmode'])==3: label += 'flow+obj+rgb'
+    elif int(d['speedmode'])==4: label += 'rgb'
+    label += '_'
+    if int(d['convmode'])==0: label += 'baseline-cnn'
+    elif int(d['convmode'])==1: label += 'resnet'
+    elif int(d['convmode'])==2: label += 'alexnet'
+
+    return label
+
+def plot_loss(**options):
+    plt.figure(1, figsize=(8,3))
+    ax1 = plt.subplot(1,2,1)
+    handles = []
+    for log in logs:
+        label = lookup(results[log])
+        handles += (ax1.plot(results[log]['epoch'], results[log]['train_mse'], label=label))
+    ax1.set_title('train_loss')
+    ax1.grid(True)
+    ax1.set_xlabel('epoch')
+    ax2 = plt.subplot(1,2,2)
+    handles = []
+    for log in logs:
+        label = lookup(results[log])
+        handles += (ax2.plot(results[log]['epoch'], results[log]['val_mse'], label=label))
+    ax2.set_title('val_loss')
+    ax2.grid(True)
+    ax2.set_xlabel('epoch')
+    ax2.legend(handles=handles)
+    plt.gcf().subplots_adjust(bottom=0.15)
+    plt.savefig('{}/loss.png'.format(options['path']))
+
+def main():
+    usage = "Usage: plot [options --path]"
+    parser = argparse.ArgumentParser(description='Visualize a sequence of images as video')
+    parser.add_argument('--path', dest='path', action='store', default='../results',
+            help='Specify path for result logs')
+    (options, args) = parser.parse_known_args()
+
+    options = vars(options)
+    parse(**options)
+    plot_loss(**options)
+
+if __name__ == "__main__":
+    main()
